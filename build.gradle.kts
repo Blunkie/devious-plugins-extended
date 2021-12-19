204c6a33fe542add26fb93a06b0337fde7979af3
Burak <burakinan1@hotmail.com>
2021-12-19 14:38:06 +0100

    Initial setup

---
+++ build.gradle.kts
@@ -0,0 +1,77 @@
+import ProjectVersions.openosrsVersion
+
+buildscript {
+    repositories {
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    `java-library`
+    checkstyle
+}
+
+project.extra["GithubUrl"] = "https://github.com/buracc/unethicalite-plugins"
+
+apply<BootstrapPlugin>()
+
+subprojects {
+    group = "dev.unethical"
+
+    project.extra["PluginProvider"] = "unethicalite"
+    project.extra["ProjectSupportUrl"] = "https://discord.gg/WTvTbSPknJ"
+    project.extra["PluginLicense"] = "3-Clause BSD License"
+
+    apply<JavaPlugin>()
+    apply(plugin = "java-library")
+    apply(plugin = "checkstyle")
+
+    repositories {
+        jcenter {
+            content {
+                excludeGroupByRegex("com\\.openosrs.*")
+            }
+        }
+
+        exclusiveContent {
+            forRepository {
+                mavenLocal()
+            }
+
+            filter {
+                includeGroupByRegex("com\\.openosrs.*")
+            }
+        }
+    }
+
+    dependencies {
+        annotationProcessor(Libraries.lombok)
+        annotationProcessor(Libraries.pf4j)
+
+        compileOnly("com.openosrs:runelite-api:$openosrsVersion+")
+        compileOnly("com.openosrs:runelite-client:$openosrsVersion+")
+
+        compileOnly(Libraries.guice)
+        compileOnly(Libraries.javax)
+        compileOnly(Libraries.lombok)
+        compileOnly(Libraries.pf4j)
+    }
+
+    configure<JavaPluginConvention> {
+        sourceCompatibility = JavaVersion.VERSION_16
+        targetCompatibility = JavaVersion.VERSION_16
+    }
+
+    tasks {
+        withType<JavaCompile> {
+            options.encoding = "UTF-8"
+        }
+
+        withType<AbstractArchiveTask> {
+            isPreserveFileTimestamps = false
+            isReproducibleFileOrder = true
+            dirMode = 493
+            fileMode = 420
+        }
+    }
+}
