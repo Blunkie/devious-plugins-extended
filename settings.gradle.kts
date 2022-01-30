248235a8251cf404b8a26af951e921f59af87633
Burak <burakinan1@hotmail.com>
2022-01-30 16:46:08 +0100

    Add hoot notifier

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -35,6 +35,7 @@ include("hoot-trawler")
 include("hoot-pickpocket")
 include("hoot-aerialfishing")
 include("hoot-chins")
+include("hoot-notifier")
 include("interaction-tester")
 
 for (project in rootProject.children) {
