ddb99d0c36e31aab82622da35b90f44f1bee6cc9
Burak <burakinan1@hotmail.com>
2022-05-03 23:27:24 +0200

    Add logging to prayer plugin

--- hoot-fighter/src/main/java/dev/hoot/fighter/AlchSpell.java
+++ hoot-fighter/src/main/java/dev/hoot/fighter/AlchSpell.java
@@ -1,28 +1,28 @@
 package dev.hoot.fighter;
 
 import dev.unethicalite.api.game.Skills;
-import dev.unethicalite.api.magic.Regular;
 import dev.unethicalite.api.magic.Rune;
 import dev.unethicalite.api.magic.RuneRequirement;
+import dev.unethicalite.api.magic.SpellBook;
 import net.runelite.api.Skill;
 
 import java.util.Arrays;
 
 public enum AlchSpell
 {
-	HIGH(Regular.HIGH_LEVEL_ALCHEMY, new RuneRequirement(1, Rune.NATURE), new RuneRequirement(5, Rune.FIRE)),
-	LOW(Regular.LOW_LEVEL_ALCHEMY, new RuneRequirement(1, Rune.NATURE), new RuneRequirement(1, Rune.FIRE));
+	HIGH(SpellBook.Standard.HIGH_LEVEL_ALCHEMY, new RuneRequirement(1, Rune.NATURE), new RuneRequirement(5, Rune.FIRE)),
+	LOW(SpellBook.Standard.LOW_LEVEL_ALCHEMY, new RuneRequirement(1, Rune.NATURE), new RuneRequirement(1, Rune.FIRE));
 
-	private final Regular spell;
+	private final SpellBook.Standard spell;
 	private final RuneRequirement[] requirements;
 
-	AlchSpell(Regular spell, RuneRequirement... requirements)
+	AlchSpell(SpellBook.Standard spell, RuneRequirement... requirements)
 	{
 		this.spell = spell;
 		this.requirements = requirements;
 	}
 
-	public Regular getSpell()
+	public SpellBook.Standard getSpell()
 	{
 		return spell;
 	}
--- unethical-prayer/src/main/java/dev/unethicalite/plugins/prayer/UnethicalPrayerPlugin.java
+++ unethical-prayer/src/main/java/dev/unethicalite/plugins/prayer/UnethicalPrayerPlugin.java
@@ -1,6 +1,7 @@
 package dev.unethicalite.plugins.prayer;
 
 import dev.unethicalite.api.widgets.Prayers;
+import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Actor;
 import net.runelite.api.AnimationID;
 import net.runelite.api.Prayer;
@@ -12,6 +13,7 @@ import org.pf4j.Extension;
 
 @Extension
 @PluginDescriptor(name = "Unethical Prayer", enabledByDefault = false)
+@Slf4j
 public class UnethicalPrayerPlugin extends Plugin
 {
 	public static final int JALTOK_JAD_MAGE_ATTACK = 7592;
@@ -30,16 +32,22 @@ public class UnethicalPrayerPlugin extends Plugin
 		switch (actor.getAnimation())
 		{
 			case AnimationID.TZTOK_JAD_MAGIC_ATTACK:
+				log.info("TzTok Jad mage attack");
 			case JALTOK_JAD_MAGE_ATTACK:
+				log.info("JalTok Jad mage attack");
 				if (!Prayers.isEnabled(Prayer.PROTECT_FROM_MAGIC))
 				{
+					log.info("Enabling protection from magic");
 					Prayers.toggle(Prayer.PROTECT_FROM_MAGIC);
 				}
 				break;
 			case AnimationID.TZTOK_JAD_RANGE_ATTACK:
+				log.info("TzTok Jad range attack");
 			case JALTOK_JAD_RANGE_ATTACK:
+				log.info("JalTok Jad range attack");
 				if (!Prayers.isEnabled(Prayer.PROTECT_FROM_MISSILES))
 				{
+					log.info("Enabling protection from missiles");
 					Prayers.toggle(Prayer.PROTECT_FROM_MISSILES);
 				}
 				break;
