5feac86f54192f22ba004c84d742b1425886bfaa
Burak <burakinan1@hotmail.com>
2022-07-08 14:34:04 +0200

    project: unethicalite version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-import ProjectVersions.openosrsVersion
+import ProjectVersions.unethicaliteVersion
 
 buildscript {
     repositories {
@@ -52,8 +52,8 @@ allprojects {
         annotationProcessor(Libraries.lombok)
         annotationProcessor(Libraries.pf4j)
 
-        compileOnly("com.openosrs:runelite-api:$openosrsVersion+")
-        compileOnly("com.openosrs:runelite-client:$openosrsVersion+")
+        compileOnly("com.openosrs:runelite-api:$unethicaliteVersion+")
+        compileOnly("com.openosrs:runelite-client:$unethicaliteVersion+")
 
         compileOnly(Libraries.guice)
         compileOnly(Libraries.javax)
--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -24,7 +24,7 @@
  */
 
 object ProjectVersions {
-    const val openosrsVersion = "4.31.0"
+    const val unethicaliteVersion = "1.0.0"
     const val apiVersion = "^1.0.0"
 }
 
