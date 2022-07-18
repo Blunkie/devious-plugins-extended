836c7a3e138d28a5cd9d439eaa7752df9aff8ee7
Burak <burakinan1@hotmail.com>
2022-07-18 16:52:26 +0200

    agility: bump version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -24,7 +24,7 @@
  */
 
 object ProjectVersions {
-    const val unethicaliteVersion = "1.0.1"
+    const val unethicaliteVersion = "1.0.3"
     const val apiVersion = "^1.0.0"
 }
 
--- unethical-agility/unethical-agility.gradle.kts
+++ unethical-agility/unethical-agility.gradle.kts
@@ -1,4 +1,4 @@
-version = "0.0.2"
+version = "0.0.3"
 
 project.extra["PluginName"] = "Unethical Agility"
 project.extra["PluginDescription"] = "Completes Rooftop agility courses"
