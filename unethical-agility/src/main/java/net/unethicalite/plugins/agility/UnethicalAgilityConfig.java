98fc7b7984cf5a585317de6cd3de3793649b910e
DevKristoffer <krsvanstr@gmail.com>
2022-08-05 12:36:48 +0200

    Changes to Unethical Agility

--- unethical-agility/src/main/java/net/unethicalite/plugins/agility/UnethicalAgilityConfig.java
+++ unethical-agility/src/main/java/net/unethicalite/plugins/agility/UnethicalAgilityConfig.java
@@ -29,4 +29,79 @@ public interface UnethicalAgilityConfig extends Config
 	{
 		return 75;
 	}
+
+	@ConfigItem(
+			keyName = "useSummerPies",
+			name = "Use Summer Pies",
+			description = "Enable using Summer Pies to boost Agility",
+			position = 20
+	)
+	default boolean useSummerPies()
+	{
+		return false;
+	}
+
+	@Range(min = 1, max = 5)
+	@ConfigItem(
+			keyName = "minBoostAmount",
+			name = "Min Boost Amount",
+			description = "The minimum amount you want your Agility to be boosted",
+			position = 30,
+			hidden = true,
+			unhide = "useSummerPies"
+	)
+	default int minBoostAmount()
+	{
+		return 1;
+	}
+
+	@ConfigItem(
+			keyName = "stopWhenOutOfSummerPies",
+			name = "Stop when out of Summer Pies",
+			description = "Enable this to prevent trying to do a course you don't have the Agility level for",
+			position = 40,
+			hidden = true,
+			unhide = "useSummerPies"
+	)
+	default boolean stopWhenOutOfSummerPies()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+			keyName = "useStaminas",
+			name = "Use Staminas",
+			description = "Uses Stamina potions if there are any in your inventory",
+			position = 50
+	)
+	default boolean useStaminas()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+			keyName = "minEnergyAmount",
+			name = "Min",
+			description = "Minimum energy to boost at",
+			position = 60,
+			hidden = true,
+			unhide = "useStaminas"
+	)
+	default int minEnergyAmount()
+	{
+		return 20;
+	}
+
+	@ConfigItem(
+			keyName = "maxEnergyAmount",
+			name = "Max",
+			description = "Maximum energy to boost at",
+			position = 70,
+			hidden = true,
+			unhide = "useStaminas"
+	)
+	default int maxEnergyAmount()
+	{
+		return 40;
+	}
 }
