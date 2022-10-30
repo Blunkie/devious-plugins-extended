2ddea39e0b463f71ffb4377dd675ac2bf475d31b
Burak <burakinan1@hotmail.com>
2022-10-30 20:37:12 +0100

    prayer: add more black demons

---
+++ unethical-prayer/src/main/java/net/unethicalite/plugins/prayer/MonsterID.java
@@ -0,0 +1,12 @@
+package net.unethicalite.plugins.prayer;
+
+import net.runelite.api.NpcID;
+
+public interface MonsterID
+{
+	int[] BLACK_DEMONS =
+			{
+					NpcID.BLACK_DEMON_2049, NpcID.BLACK_DEMON_2048, NpcID.BLACK_DEMON_2050, NpcID.BLACK_DEMON_2051,
+					NpcID.BLACK_DEMON_2052, NpcID.BLACK_DEMON_7242, NpcID.BLACK_DEMON_7243,
+			};
+}
