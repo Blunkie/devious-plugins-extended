945fd1d555df38007cba5cfd6c8e96d78fac0d08
BloatGoat <Bloat@goat.com>
2022-11-19 22:22:35 +0100

    Add teak, maple, mahogany, yew and magic tree support
    (cherry picked from commit 60d6ec4a952bccf6650cc2cc6983dde522f8333d)

--- unethical-chopper/src/main/java/net/unethicalite/plugins/chopper/Tree.java
+++ unethical-chopper/src/main/java/net/unethicalite/plugins/chopper/Tree.java
@@ -5,9 +5,12 @@ import lombok.Getter;
 @Getter
 public enum Tree
 {
-	REGULAR(1, "Tree", "Evergreen"),
-	OAK(15, "Oak"),
-	WILLOW(30, "Willow");
+	WILLOW(30, "Willow"),
+	TEAK(35, "Teak"),
+	MAPLE(45, "Maple tree"),
+	MAHOGANY(50, "Mahogany"),
+	YEW(60, "Yew"),
+	MAGIC(75, "Magic tree");
 
 	private final int level;
 	private final String[] names;
