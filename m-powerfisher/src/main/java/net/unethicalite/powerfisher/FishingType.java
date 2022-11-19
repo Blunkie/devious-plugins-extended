2f6225b55e5e7014f748be96f29e0ba744924773
melxin <melxin@tutamail.com>
2022-11-19 14:53:05 +0100

    powerfisher: fix required item names

--- m-powerfisher/src/main/java/net/unethicalite/powerfisher/FishingType.java
+++ m-powerfisher/src/main/java/net/unethicalite/powerfisher/FishingType.java
@@ -8,9 +8,9 @@ import net.runelite.api.ItemID;
 @Getter
 public enum FishingType
 {
-	SHRIMPS_AND_ANCHOVIES(new String[]{"net"}, new int[]{ItemID.RAW_SHRIMPS, ItemID.RAW_ANCHOVIES}, "Net"),
-	TROUT_AND_SALMON(new String[]{"rod", "feathers"}, new int[]{ItemID.RAW_TROUT, ItemID.RAW_SALMON}, "Lure"),
-	BARBARIAN_ROD(new String[]{"rod", "feathers"}, new int[]{ItemID.LEAPING_TROUT, ItemID.LEAPING_SALMON, ItemID.LEAPING_STURGEON}, "Use-rod");
+	SHRIMPS_AND_ANCHOVIES(new String[]{"Small fishing net"}, new int[]{ItemID.RAW_SHRIMPS, ItemID.RAW_ANCHOVIES}, "Net"),
+	TROUT_AND_SALMON(new String[]{"Fly fishing rod", "Feather"}, new int[]{ItemID.RAW_TROUT, ItemID.RAW_SALMON}, "Lure"),
+	BARBARIAN_ROD(new String[]{"Barbarian rod", "Feather"}, new int[]{ItemID.LEAPING_TROUT, ItemID.LEAPING_SALMON, ItemID.LEAPING_STURGEON}, "Use-rod");
 
 	private String[] requiredItems;
 	private int[] fishToDrop;
